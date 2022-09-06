250d77c726f1699188466253ee3c1ee276feadf9
Houssambensadi <69742352+wawadz2000@users.noreply.github.com>
2022-09-06 18:02:24 +0100

    Add files via upload

---
+++ User.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pharmacy.management.system;
+
+/**
+ *
+ * @author wawad
+ */
+public class User {
+Login log = new Login();
+String Users;
+    public User(){
+        
+        final String users = log.username;
+        this.Users=users;
+        
+        
+    }
+    
+  
+    
+    
+    
+    
+    
+}
